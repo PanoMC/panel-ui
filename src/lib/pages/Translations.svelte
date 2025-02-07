5751a2330cafbc513914fec0c0076cd707b291e7
Selim Gökçek <selimgokcek@outlook.com>
2025-02-07 16:33:40 +0300

    Translations accordions

--- src/lib/pages/Translations.svelte
+++ src/lib/pages/Translations.svelte
@@ -12,20 +12,20 @@
       <CardMenuItem href="/translations/addons" startsWith>Addons</CardMenuItem>
     </CardMenu>
 
-    <div slot="right">
+    <div slot="right" class="hstack gap-2">
       <select class="form-select" name="selectLanguage" id="selectLanguage">
         <option selected value="en-us">English</option>
         <option value="tr">Turkish</option>
         <option value="ru">Russian</option>
       </select>
+      <button class="btn btn-secondary disabled" disabled>Save</button>
     </div>
   </PageActions>
 
   <div class="card">
-    <div class="card-body">
+    <div class="card-body vstack gap-3">
       <CardHeader>
-        <h5 class="card-title" slot="left">Platform Translations (102)</h5>
-
+        <h5 class="card-title" slot="left">Platform Translations (307)</h5>
         <!-- Filters -->
         <CardFilters slot="right">
           <CardFiltersItem href="/" active>All</CardFiltersItem>
@@ -33,6 +33,86 @@
           <CardFiltersItem href="/">Modified</CardFiltersItem>
         </CardFilters>
       </CardHeader>
+
+      <div class="accordion">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseplatformTranslations">
+              tr.json (102)
+            </button>
+          </h2>
+          <div
+            id="collapseplatformTranslations"
+            class="accordion-collapse collapse show">
+            <div class="accordion-body">
+              <div class="row-cols-3 g-2 d-flex flex-nowrap">
+                <label for="keyTranslation">Key</label>
+                <label for="originalTranslation">Original</label>
+                <label for="customTranslation">Custom</label>
+                <label for=""></label>
+              </div>
+              <div
+                class="input-group row-cols-3 d-flex flex-nowrap g-2 overflow-x-auto">
+                <span
+                  class="input-group-text font-monospace"
+                  id="keyTranslation">DATA</span>
+                <input
+                  type="text"
+                  class="form-control disabled"
+                  disabled
+                  id="originalTranslation" />
+                <input
+                  type="text"
+                  class="form-control border-danger"
+                  id="customTranslation" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOtherTranslations">
+              Other (1)
+            </button>
+          </h2>
+          <div
+            id="collapseOtherTranslations"
+            class="accordion-collapse collapse">
+            <div class="accordion-body">
+              <div class="row-cols-3 g-2 d-flex flex-nowrap">
+                <label for="keyTranslation">Key</label>
+                <label for="originalTranslation">Original</label>
+                <label for="customTranslation">Custom</label>
+                <label for=""></label>
+              </div>
+              <div
+                class="input-group row-cols-3 d-flex flex-nowrap g-2 overflow-x-auto">
+                <span
+                  class="input-group-text font-monospace"
+                  id="keyTranslation">DATA</span>
+                <input
+                  type="text"
+                  class="form-control disabled"
+                  disabled
+                  id="originalTranslation" />
+                <input
+                  type="text"
+                  class="form-control border-danger"
+                  id="customTranslation" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
