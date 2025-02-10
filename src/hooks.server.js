caa52c5128985f0d2ea82ba85cee018eeffeeeed
kahverengi <ahmet@duruer.dev>
2025-02-11 00:27:31 +0300

    feat: add setting origin header on back-end server fetch requests

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -41,6 +41,7 @@ export async function handle({ event, event: { cookies }, resolve }) {
 export async function handleFetch({ event, request, fetch }) {
   if (request.url.startsWith(API_URL)) {
     request.headers.set('cookie', event.request.headers.get('cookie'));
+    request.headers.set("Origin", API_URL);
   }
 
   return fetch(request);
