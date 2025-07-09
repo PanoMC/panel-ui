959f1273f52fd19a2b0b6dd939d5233e8d47bab9
kahverengi <ahmet@duruer.dev>
2025-07-09 14:56:30 +0300

    fix: fix not working markdown, also add cheerio

--- src/lib/component/MarkdownRenderer.svelte
+++ src/lib/component/MarkdownRenderer.svelte
@@ -2,28 +2,42 @@
   import { sanitize } from '@jill64/universal-sanitizer'
   import { marked } from "marked";
 
+  import { browser } from "$app/environment";
+
   marked.use({
     gfm: true
   })
 
   export let content
 
-  function removeFirstPTag(html) {
-    const container = document.createElement('div');
-    container.innerHTML = html;
+  async function removeFirstPTag(html) {
+    if (browser) { // CSR
+      const container = document.createElement('div');
+      container.innerHTML = html;
+      const firstChild = container.firstElementChild;
 
-    const firstChild = container.firstElementChild;
-    if (firstChild && firstChild.tagName === 'P') {
-      // Replace the <p> with its children
-      const fragment = document.createDocumentFragment();
-      while (firstChild.firstChild) {
-        fragment.appendChild(firstChild.firstChild);
+      if (firstChild && firstChild.tagName === 'P') {
+        const fragment = document.createDocumentFragment();
+        while (firstChild.firstChild) {
+          fragment.appendChild(firstChild.firstChild);
+        }
+        container.replaceChild(fragment, firstChild);
       }
-      container.replaceChild(fragment, firstChild);
-    }
 
-    return container.innerHTML;
+      return container.innerHTML;
+    } else { // SSR
+      const cheerio = await import('cheerio');
+      const $ = cheerio.load(html);
+      const p = $('body').children().first();
+
+      if (p.is('p')) {
+        return p.html();
+      }
+      return html;
+    }
   }
 </script>
 
-{@html removeFirstPTag(marked.parse(sanitize(content)))}
+{#await removeFirstPTag(marked.parse(sanitize(content))) then content}
+  {@html content}
+{/await}
