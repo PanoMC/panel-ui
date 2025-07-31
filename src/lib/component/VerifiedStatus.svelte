30b2baafa0d822336442a6f1ef3c1b9a6c68856f
kahverengi <ahmet@duruer.dev>
2025-07-31 13:39:44 +0300

    Separate verify status component

---
+++ src/lib/component/VerifiedStatus.svelte
@@ -0,0 +1,26 @@
+{#if status !== "UNKNOWN"}{/if}
+{#if status === "VERIFIED"}
+                        <span
+                          class="text-success"
+                          use:tooltip={[
+                            "Verified by Pano",
+                            { placement: "bottom" },
+                          ]}>
+                          <i class="fa-regular fa-circle-check"></i>
+                        </span>
+{:else if status === "NOT_VERIFIED"}
+  <div
+    class="text-warning"
+    use:tooltip={[
+                            "Not verified by Pano, use at your own risk!",
+                            { placement: "bottom" },
+                          ]}>
+    <i class="fa-solid fa-circle-exclamation me-1"></i>
+  </div>
+{/if}
+
+<script>
+  import tooltip from "$lib/tooltip.util"
+
+  export let status;
+</script>
--- src/lib/pages/Addons.svelte
+++ src/lib/pages/Addons.svelte
@@ -132,26 +132,7 @@
                       {#if plugin.license}
                         <div>{plugin.license}</div>
                       {/if}
-                      {#if plugin.verifyStatus !== "UNKNOWN"}{/if}
-                      {#if plugin.verifyStatus === "VERIFIED"}
-                        <span
-                          class="text-success"
-                          use:tooltip={[
-                            "Verified by Pano",
-                            { placement: "bottom" },
-                          ]}>
-                          <i class="fa-regular fa-circle-check"></i>
-                        </span>
-                      {:else if plugin.verifyStatus === "NOT_VERIFIED"}
-                        <div
-                          class="text-warning"
-                          use:tooltip={[
-                            "Not verified by Pano, use at your own risk!",
-                            { placement: "bottom" },
-                          ]}>
-                          <i class="fa-solid fa-circle-exclamation me-1"></i>
-                        </div>
-                      {/if}
+                      <VerifiedStatus status="{plugin.verifyStatus}"/>
                     </div>
                   </div>
                 </div>
@@ -213,7 +194,7 @@
 
   import { base } from "$app/paths";
 
-  import { API_URL, PANO_WEBSITE_URL } from "$lib/variables";
+  import { PANO_WEBSITE_URL } from "$lib/variables";
   import tooltip from "$lib/tooltip.util";
 
   import { show as showToast } from "$lib/component/ToastContainer.svelte";
@@ -235,6 +216,7 @@
   } from "$lib/component/modals/ConfirmEnablingAddonWillCauseMoreEnableModal.svelte";
 
   import NoContent from "$lib/component/NoContent.svelte";
+  import VerifiedStatus from "$lib/component/VerifiedStatus.svelte";
 
   export let data;
 
