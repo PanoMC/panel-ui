8062c5936a7d185e40bb1af30c390044ea589f04
Selim Gökçek <selimgokcek@outlook.com>
2025-07-31 22:03:32 +0300

    fix: add dynamic class support for CardHeader component

--- src/lib/component/CardHeader.svelte
+++ src/lib/component/CardHeader.svelte
@@ -1,12 +1,18 @@
 <div class="card-header">
   <div class="row d-flex align-items-center gx-0 gy-2">
     <div
-      class="col-lg-6 d-flex justify-content-lg-between justify-content-center">
+      class="col-lg-6 d-flex justify-content-lg-between justify-content-center {leftClasses}">
       <slot name="left" />
     </div>
-    <div class="col-lg-6 d-flex justify-content-lg-end justify-content-center">
+    <div
+      class="col-lg-6 d-flex justify-content-lg-end justify-content-center {rightClasses}">
       <!-- Filters -->
       <slot name="right" />
     </div>
   </div>
 </div>
+
+<script>
+  export let leftClasses;
+  export let rightClasses;
+</script>
