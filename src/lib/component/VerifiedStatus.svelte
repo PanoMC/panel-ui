24c80b1d76bb28098211eed4c7e1639664918998
kahverengi <ahmet@duruer.dev>
2025-08-03 22:35:43 +0300

    fix: fxi verify status not properly aligning due to div used instead of span

--- src/lib/component/VerifiedStatus.svelte
+++ src/lib/component/VerifiedStatus.svelte
@@ -1,22 +1,22 @@
 {#if status !== "UNKNOWN"}{/if}
 {#if status === "VERIFIED"}
-                        <span
-                          class="text-success"
-                          use:tooltip={[
-                            $_('components.verified-status.verified'),
-                            { placement: "bottom" },
-                          ]}>
-                          <i class="fa-regular fa-circle-check"></i>
-                        </span>
+  <span
+    class="text-success"
+    use:tooltip={[
+      $_('components.verified-status.verified'),
+      { placement: "bottom" },
+    ]}>
+    <i class="fa-regular fa-circle-check"></i>
+  </span>
 {:else if status === "NOT_VERIFIED"}
-  <div
+  <span
     class="text-warning"
     use:tooltip={[
                             $_('components.verified-status.not-verified'),
                             { placement: "bottom" },
                           ]}>
     <i class="fa-solid fa-circle-exclamation me-1"></i>
-  </div>
+  </span>
 {/if}
 
 <script>
