19355ec16282629bcd9c0473caad52bc50482d5b
kahverengi <ahmet@duruer.dev>
2025-08-06 12:37:36 +0300

    feat: improve date component by adding new features

--- src/lib/component/Date.svelte
+++ src/lib/component/Date.svelte
@@ -1,16 +1,20 @@
 <span
-  use:tooltip="{[
+  use:useTooltip="{tooltip && [
     format(new Date(parseInt(date)), 'dd/MM/yyyy, HH:mm'),
     { placement: 'bottom', locale: locales[$currentLanguage.dateFnsCode] },
   ]}">
   <slot>
     {#if relativeFormat}
       {formatRelative(new Date(parseInt(date)), new Date(), {
-        locale: locales[$currentLanguage.dateFnsCode],
+        locale: locales[$currentLanguage.dateFnsCode]
       }).capitalize()}
+    {:else if fullFormat}
+      {format(new Date(parseInt(date)), 'dd/MM/yyyy, HH:mm', {
+        locale: locales[$currentLanguage.dateFnsCode]
+      })}
     {:else}
       {format(new Date(parseInt(date)), "dd MMMM yyyy", {
-        locale: locales[$currentLanguage.dateFnsCode],
+        locale: locales[$currentLanguage.dateFnsCode]
       })}
     {/if}
   </slot>
@@ -20,7 +24,7 @@
   import { format, formatRelative } from "date-fns";
   import * as locales from "date-fns/locale";
 
-  import tooltip from "$lib/tooltip.util";
+  import useTooltip from "$lib/tooltip.util";
   import { currentLanguage } from "$lib/language.util.js";
 
   String.prototype.capitalize = function () {
@@ -28,7 +32,10 @@
   };
 
   export let time;
-  export let relativeFormat = true;
+  export let relativeFormat = false;
+  export let fullFormat = false;
+  export let tooltip = true;
 
   $: date = time;
 </script>
+
