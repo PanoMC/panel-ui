824341e91884f830e4f6012280a0a441589d2f2f
kahverengi <ahmet@duruer.dev>
2025-10-24 12:52:51 +0300

    fix: fixed after removing a server goes to not correct place

--- src/lib/layouts/ServerLayout.svelte
+++ src/lib/layouts/ServerLayout.svelte
@@ -4,6 +4,8 @@
   import { redirect } from "@sveltejs/kit";
   import { hasPermission, Permissions } from "$lib/auth.util.js";
 
+  import { base } from "$app/paths";
+
   /**
    * @type {import('@sveltejs/kit').LayoutLoad}
    */
@@ -12,11 +14,11 @@
     const { user, selectedServer } = parentData;
 
     if (!hasPermission(Permissions.MANAGE_SERVERS, user)) {
-      throw redirect(302, "/");
+      throw redirect(302, base);
     }
 
     if (!selectedServer) {
-      throw redirect(302, "/");
+      throw redirect(302, base);
     }
 
     return parentData;
