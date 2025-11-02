3d2d9baa77d9399250c3df59dc985bb71d620293
kahverengi <ahmet@duruer.dev>
2025-11-02 22:22:48 +0300

    moved migration to under settings page

--- src/routes/settings/migration/+page.svelte
+++ src/routes/settings/migration/+page.svelte
@@ -1,7 +1,7 @@
 <Migration {data} />
 
 <script>
-  import Migration from "$lib/pages/Migration.svelte";
+  import Migration from "$lib/pages/settings/Migration.svelte";
 
   export let data;
 </script>
