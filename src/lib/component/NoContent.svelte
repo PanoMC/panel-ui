7321b345e783dcabc68c8bfd6c398cac41d764af
kahverengi <ahmet@duruer.dev>
2025-11-04 21:49:17 +0300

    fix: removed dark/light logic from no content component

--- src/lib/component/NoContent.svelte
+++ src/lib/component/NoContent.svelte
@@ -1,28 +1,16 @@
-{#if dark}
-  <div
-    class="card border-0 opacity-50 bg-transparent animate__animated animate__zoomIn"
-    data-bs-theme="dark">
-    <div class="card-body vstack gap-3 text-center">
-      <span class="{icon}"></span>
-      <p class="mb-0">{text}</p>
-    </div>
+<div
+  class="card border-0 opacity-50 bg-transparent animate__animated animate__zoomIn">
+  <div class="card-body vstack gap-3 text-center">
+    <span class="{icon}"></span>
+    <p class="mb-0">{text}</p>
   </div>
-{:else}
-  <div
-    class="card border-0 opacity-50 bg-transparent animate__animated animate__zoomIn">
-    <div class="card-body vstack gap-3 text-center">
-      <span class="{icon}"></span>
-      <p class="mb-0">{text}</p>
-    </div>
-  </div>
-{/if}
+</div>
 
 <slot />
 
 <script>
   import { _ } from "svelte-i18n";
 
-  export let dark = false;
   export let icon = "fa-solid fa-ghost fa-3x";
   export let text = $_("components.no-content.here-is-empty");
 </script>
