17ba3c8e0a5f9da2dc0b6c63c80ce5f59f99d713
kahverengi <ahmet@duruer.dev>
2025-11-13 18:38:04 +0300

    fix: fixed not getting correctly licenses.josn

--- src/routes/panel-api/licenses.json/+server.js
+++ src/routes/panel-api/licenses.json/+server.js
@@ -3,12 +3,10 @@ import path from "path";
 
 /** @type {import("@sveltejs/kit").RequestHandler} */
 export async function GET() {
-  // Önce build klasöründen oku
-  let filePath = path.resolve(process.cwd(), "build", "licenses.json");
+  let filePath = path.resolve(`licenses.json`);
 
-  // Build klasöründe yoksa ana dizinden oku (development için)
   if (!fs.existsSync(filePath)) {
-    filePath = path.resolve(process.cwd(), "licenses.json");
+    filePath = path.resolve(path.dirname(process.argv[1]) + `licenses.json`);
   }
 
   if (fs.existsSync(filePath)) {
