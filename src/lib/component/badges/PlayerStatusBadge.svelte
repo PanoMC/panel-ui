81e6514e74f282f9f2d8641c795d7ca3427aa89a
Selim Gökçek <selimgokcek@outlook.com>
2025-11-25 12:20:41 +0300

    fix: update PlayerStatusBadge color for improved status indication

--- src/lib/component/badges/PlayerStatusBadge.svelte
+++ src/lib/component/badges/PlayerStatusBadge.svelte
@@ -17,7 +17,7 @@
   </div>
 {:else}
   <div
-    class="badge rounded-pill text-bg-danger"
+    class="badge rounded-pill text-bg-primary"
     use:tooltip="{[
       getOfflineRelativeDateText(checkTime),
       { placement: 'bottom' },
