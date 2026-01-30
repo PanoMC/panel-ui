a749633ae3773cf8690bb5678d5d5238c3df7d29
kahverengi <ahmet@duruer.dev>
2026-01-30 16:52:40 +0300

    feat: improved add edit language modal for better understanding

--- src/lib/component/modals/AddEditLanguageModal.svelte
+++ src/lib/component/modals/AddEditLanguageModal.svelte
@@ -17,59 +17,95 @@
       </div>
       <form on:submit|preventDefault={onSubmit}>
         <div class="modal-body">
-          <input
-            class="form-control form-control-lg mb-3"
-            placeholder={$_('components.modals.add-edit-language.inputs.name')}
-            id="name"
-            type="text"
-            bind:value={$locale.name}
-            on:input={onNameChange}
-            class:border-danger={$error === 'INVALID_LOCALE_NAME'} />
-          <input
-            class="form-control mb-3"
-            placeholder={$_('components.modals.add-edit-language.inputs.code')}
-            id="code"
-            type="text"
-            bind:value={$locale.code}
-            class:border-danger={$error === 'INVALID_LOCALE_CODE'} />
-          <input
-            class="form-control mb-3"
-            placeholder={$_('components.modals.add-edit-language.inputs.date-fns-code')}
-            id="dateFnsCode"
-            type="text"
-            bind:value={$locale.dateFnsCode}
-            class:border-danger={$error === 'INVALID_DATE_FNS_CODE'} />
-
-          <input
-            id="derivatives"
-            class="form-control"
-            class:border-danger={$error === 'derivatives' || $error === 'INVALID_LOCALE_DERIVATIVE'}
-            placeholder={$_('components.modals.add-edit-language.inputs.derivatives')}
-            type="text"
-            name="derivative"
-            bind:value={$derivative}
-            on:input={onDerivativeChange}
-            on:keydown={(e) => {
-              if (e.key === 'Enter') {
-                e.preventDefault();
-                addKeyWord();
-              }
-            }} />
-          {#if $locale.derivatives.length > 0}
-            <div class="hstack gap-2 flex-wrap">
-              {#each $locale.derivatives as derivative, index (derivative)}
-                <button
-                  type="button"
-                  class="btn btn-link p-0 d-inline-block mt-2 text-decoration-none"
-                  use:tooltip={[$_('buttons.remove'), { placement: 'bottom' }]}
-                  on:click={() => removeKeyWord(index)}>
-                  <span class="badge text-bg-primary">
-                    {derivative}
-                  </span>
-                </button>
-              {/each}
+          <div class="row">
+            <div class="col-12 mb-3">
+              <label for="name">{$_('components.modals.add-edit-language.inputs.name')}</label>
+              <input
+                class="form-control form-control-lg"
+                id="name"
+                type="text"
+                bind:value={$locale.name}
+                on:input={onNameChange}
+                class:is-invalid={$error === 'INVALID_LOCALE_NAME'} />
             </div>
-          {/if}
+            <div class="col-6 mb-3">
+              <label for="code">{$_('components.modals.add-edit-language.inputs.code')}</label>
+              <input
+                class="form-control"
+                id="code"
+                type="text"
+                bind:value={$locale.code}
+                class:is-invalid={$error === 'INVALID_LOCALE_CODE'} />
+              <div class="form-text">
+                {$_('components.modals.add-edit-language.inputs.code-helper')}
+                <a
+                  href={$_('components.modals.add-edit-language.inputs.code-lookup-url')}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="text-decoration-none">
+                  <i class="fa-solid fa-external-link small ms-1"></i>
+                  {$_('components.modals.add-edit-language.inputs.code-lookup-text')}
+                </a>
+              </div>
+            </div>
+            <div class="col-6 mb-3">
+              <label for="dateFnsCode">
+                {$_('components.modals.add-edit-language.inputs.date-fns-code')}
+              </label>
+              <select
+                class="form-control"
+                id="dateFnsCode"
+                bind:value={$locale.dateFnsCode}
+                class:is-invalid={$error === 'INVALID_DATE_FNS_CODE'}>
+                <option value="">{$_('buttons.select-option')}</option>
+                {#each Object.keys(dateLocales).sort() as dateLocaleKey}
+                  <option value={dateLocales[dateLocaleKey].code || dateLocaleKey}>
+                    {dateLocales[dateLocaleKey].code || dateLocaleKey}
+                  </option>
+                {/each}
+              </select>
+              <div class="form-text">
+                {$_('components.modals.add-edit-language.inputs.date-fns-code-helper')}
+              </div>
+            </div>
+
+            <div class="col-12 mb-3">
+              <label for="derivatives">{$_('pages.languages.derivatives')}</label>
+              <input
+                id="derivatives"
+                class="form-control"
+                class:is-invalid={$error === 'derivatives' || $error === 'INVALID_LOCALE_DERIVATIVE'}
+                placeholder={$_('components.modals.add-edit-language.inputs.derivatives')}
+                type="text"
+                name="derivative"
+                bind:value={$derivative}
+                on:input={onDerivativeChange}
+                on:keydown={(e) => {
+                  if (e.key === 'Enter') {
+                    e.preventDefault();
+                    addKeyWord();
+                  }
+                }} />
+              <div class="form-text">
+                {$_('components.modals.add-edit-language.inputs.derivatives-helper')}
+              </div>
+              {#if $locale.derivatives.length > 0}
+                <div class="hstack gap-2 flex-wrap">
+                  {#each $locale.derivatives as derivative, index (derivative)}
+                    <button
+                      type="button"
+                      class="btn btn-link p-0 d-inline-block mt-2 text-decoration-none"
+                      use:tooltip={[$_('buttons.remove'), { placement: 'bottom' }]}
+                      on:click={() => removeKeyWord(index)}>
+                      <span class="badge text-bg-primary">
+                        {derivative}
+                      </span>
+                    </button>
+                  {/each}
+                </div>
+              {/if}
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
           <button
@@ -140,6 +176,7 @@
 </script>
 
 <script>
+  import * as dateLocales from 'date-fns/locale';
   import { _ } from 'svelte-i18n';
 
   import ApiUtil from '$lib/api.util';
