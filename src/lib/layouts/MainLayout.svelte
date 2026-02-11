10216ff5f3ad78fa0258a365c82e54427257aa32
kahverengi <ahmet@duruer.dev>
2026-02-12 00:13:33 +0300

    feat: added demo mode support

--- src/lib/layouts/MainLayout.svelte
+++ src/lib/layouts/MainLayout.svelte
@@ -2,6 +2,19 @@
 
 <!--  Main  -->
 <main class="w-100 min-vh-100 overflow-auto pb-5 d-flex flex-column">
+  {#if $siteInfo?.isDemo}
+    <div
+      class="alert alert-info fade show rounded-0 border-0 mb-0 shadow-sm"
+      role="alert">
+      <div class="container-fluid d-flex align-items-center">
+        <i class="fa-solid fa-circle-info me-3 fa-lg"></i>
+        <div>
+          <strong class="me-2">Demo Mode</strong>
+          <span class="small py-1">This panel is running in demo mode. Some actions are restricted.</span>
+        </div>
+      </div>
+    </div>
+  {/if}
   {#if $showDevModeAlert}
     <div
       class="alert alert-warning alert-dismissible fade show rounded-0 border-0 mb-0 shadow-sm"
@@ -32,12 +45,13 @@
 </script>
 
 <script>
+  import ApiUtil from '$lib/api.util.js';
   import { getContext } from 'svelte';
   import { _ } from 'svelte-i18n';
   import { invalidateAll } from '$app/navigation';
-  import ApiUtil from '$lib/api.util.js';
 
   const showDevModeAlert = getContext('showDevModeAlert');
+  const siteInfo = getContext('siteInfo');
 
   function dismissDevModeAlert() {
     $showDevModeAlert = false;
