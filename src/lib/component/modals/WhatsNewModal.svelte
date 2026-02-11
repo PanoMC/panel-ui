248a811e50c7b60a5fd73fdc86ad0e99b4c1f26f
Selim Gökçek <selimgokcek@outlook.com>
2026-02-11 15:40:27 +0300

    fix: update WhatsNewModal

--- src/lib/component/modals/WhatsNewModal.svelte
+++ src/lib/component/modals/WhatsNewModal.svelte
@@ -7,21 +7,47 @@
     justify-content: center;
     flex-shrink: 0;
   }
+
+  /* Override blocks paths due to /panel base path */
+  .blocks {
+    background-image:
+      url('/panel/assets/img/red.svg'), url('/panel/assets/img/yellow.svg'),
+      url('/panel/assets/img/green.svg') !important;
+  }
 </style>
 
 <div aria-hidden="true" class="modal fade" bind:this={$modalElement} role="dialog" tabindex="-1">
   <div class="modal-dialog modal-lg modal-dialog-centered" role="dialog">
     <div class="modal-content overflow-hidden">
-      <div class="modal-header border-bottom-0 pb-0">
-        <button type="button" class="btn-close" aria-label="Close" on:click={handleClose}></button>
+      <div
+        class="modal-header text-bg-primary d-block text-center border-bottom-0 py-5 position-relative blocks">
+        <button
+          type="button"
+          class="btn-close btn-close-white position-absolute top-0 end-0 m-3"
+          aria-label="Close"
+          on:click={handleClose}></button>
+        <img
+          src="{base}/assets/img/pano-logo-heorizontal.png"
+          alt="Pano"
+          class="my-3"
+          style="height: 3rem;" />
+        <p class="lead mb-0 px-lg-5">{$_('components.whats-new.description')}</p>
       </div>
-      <div class="modal-body px-lg-5 pb-5">
-        <div class="text-center mb-4">
-          <div class="display-5 mb-2">{$_('components.whats-new.title')}</div>
-          <p class="lead text-muted">{$_('components.whats-new.description')}</p>
-        </div>
-
+      <div class="modal-body px-lg-5 py-5">
         <div class="row g-4 mb-4 text-start">
+          <div class="col-md-6">
+            <div class="d-flex align-items-start">
+              <div class="feature-icon-small bg-primary bg-gradient text-white rounded-3 p-2 me-3">
+                <i class="fas fa-file-alt"></i>
+              </div>
+              <div>
+                <h5 class="fw-bold mb-1">{$_('components.whats-new.features.cms')}</h5>
+                <p class=" mb-0">
+                  {$_('components.whats-new.features.cms-description')}
+                </p>
+              </div>
+            </div>
+          </div>
           <div class="col-md-6">
             <div class="d-flex align-items-start">
               <div class="feature-icon-small bg-primary bg-gradient text-white rounded-3 p-2 me-3">
@@ -29,7 +55,7 @@
               </div>
               <div>
                 <h5 class="fw-bold mb-1">{$_('components.whats-new.features.management')}</h5>
-                <p class="small text-muted mb-0">
+                <p class=" mb-0">
                   {$_('components.whats-new.features.management-description')}
                 </p>
               </div>
@@ -42,7 +68,7 @@
               </div>
               <div>
                 <h5 class="fw-bold mb-1">{$_('components.whats-new.features.integration')}</h5>
-                <p class="small text-muted mb-0">
+                <p class=" mb-0">
                   {$_('components.whats-new.features.integration-description')}
                 </p>
               </div>
@@ -55,37 +81,16 @@
               </div>
               <div>
                 <h5 class="fw-bold mb-1">{$_('components.whats-new.features.e2ee')}</h5>
-                <p class="small text-muted mb-0">
+                <p class=" mb-0">
                   {$_('components.whats-new.features.e2ee-description')}
                 </p>
               </div>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="d-flex align-items-start">
-              <div class="feature-icon-small bg-primary bg-gradient text-white rounded-3 p-2 me-3">
-                <i class="fas fa-file-alt"></i>
-              </div>
-              <div>
-                <h5 class="fw-bold mb-1">{$_('components.whats-new.features.cms')}</h5>
-                <p class="small text-muted mb-0">
-                  {$_('components.whats-new.features.cms-description')}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="text-center">
-          <a href="{PANO_WEBSITE_URL}/docs" target="_blank" class="btn btn-secondary px-4">
-            <i class="fas fa-book me-2"></i>
-            {$_('buttons.see-docs')}
-            <i class="fas fa-external-link-alt ms-2 small"></i>
-          </a>
         </div>
       </div>
-      <div class="modal-footer d-flex justify-content-between align-items-center py-3 px-4">
-        {#if $showDonotShowAgain}
+      {#if $showDonotShowAgain}
+        <div class="modal-footer d-flex justify-content-center align-items-center">
           <div class="form-check form-switch mb-0">
             <input
               class="form-check-input"
@@ -96,11 +101,8 @@
               {$_('components.whats-new.dont-show-again')}
             </label>
           </div>
-        {/if}
-        <button class="btn btn-primary px-4" type="button" on:click={handleClose}>
-          {$_('buttons.close')}
-        </button>
-      </div>
+        </div>
+      {/if}
     </div>
   </div>
 </div>
@@ -143,6 +145,7 @@
 <script>
   import { onMount } from 'svelte';
   import { _ } from 'svelte-i18n';
+  import { base } from '$app/paths';
   import ApiUtil from '$lib/api.util.js';
   import { PANO_WEBSITE_URL } from '$lib/variables.js';
 
